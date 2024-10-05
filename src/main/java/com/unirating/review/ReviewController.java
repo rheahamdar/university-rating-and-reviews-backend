018351e0ad49ccb09f0dac2d6b9f0775ccf1b32f
rhea.hamdar <rhea.hamdar@hotmail.com>
2024-10-05 15:09:54 +0300

    added an api call to flask to check if the comment contains any profanity word

--- src/main/java/com/unirating/review/ReviewController.java
+++ src/main/java/com/unirating/review/ReviewController.java
@@ -1,10 +1,15 @@
 package com.unirating.review;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 @RestController
 @RequestMapping("/reviews")
 public class ReviewController {
@@ -16,10 +21,27 @@ public class ReviewController {
     private ReviewService reviewService;
 
     @PostMapping("/create")
-    public String createReview(@RequestBody Review review) {
+    public String createReview(@RequestBody Review review) throws IOException {
+        String comment = review.getReviewText().trim();
+        OkHttpClient client = new OkHttpClient();
+        okhttp3.RequestBody requestBody = okhttp3.RequestBody.create(MediaType.get("application/json"),
+        "{\"comment\":\"" + comment + "\"}");
+
+        Request request = new Request.Builder()
+                        .url("http://0.0.0.0:5000/check_profanity")
+                        .post(requestBody)
+                        .build();
+
+        Response response = client.newCall(request).execute();
+        String responseBody = response.body().string().trim();  
+        if (responseBody.equals("1")) {
+            return "Please remove any inappropriate word";
+        }
+
         String username = review.getUsername();
         if(username.length()>0)
             review.setUsername(username.substring(0, username.indexOf('@')));
+        review.setReviewText(comment);
         reviewRepository.save(review);
         return "Review submitted successfully";
     }
